bd45a3aabd4c439739f53c491aa1fde1ccfe9a84
alabsi <alabsi0539@gmail.com>
2025-11-21 00:08:09 +0300

    update in login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\Auth\LoginController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('me', [LoginController::class, 'me'])->middleware('auth:sanctum');
+});
