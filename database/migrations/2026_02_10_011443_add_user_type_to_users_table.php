089177db479e4e76c6a6eea17d6aabdc0d4fd6b0
alabsi <alabsi0539@gmail.com>
2026-02-20 02:15:50 +0300

    update in merchant type

--- database/migrations/2026_02_10_011443_add_user_type_to_users_table.php
+++ database/migrations/2026_02_10_011443_add_user_type_to_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserTypes;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_type')->after('email');
+            $table->string('user_type')->default(UserTypes::MERCHANT->value)->after('email');
         });
     }
 
