24e51ee67393bb3e2b4cbe0482861433a567c498
alabsi <alabsi0539@gmail.com>
2026-03-01 01:03:18 +0300

    add api notifiaction

--- routes/api.php
+++ routes/api.php
@@ -79,6 +79,8 @@ Route::prefix('v1/ecommerce')->group(function () {
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/notifications', [NotificationController::class, 'index']);
+        Route::get('/notifications/unread', [NotificationController::class, 'unread']);
+        Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
         Route::post('/notifications/read-all', [NotificationController::class, 'markAllAsRead']);
         Route::post('/notifications/read', [NotificationController::class, 'markAsRead']);
         Route::post('/notifications/send', [NotificationController::class, 'send']);
