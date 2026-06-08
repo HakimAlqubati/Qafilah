f1b5e41878f236720cb5f4761f3454a99b2fff52
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-08 10:38:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\DocsReports\RequiredReportsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    dd(auth()->user()?->vendor,auth()?->user()?->roles);
     return view('landing');
 });
 
